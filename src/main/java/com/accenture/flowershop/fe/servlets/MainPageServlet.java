dcdd264c0937e02a9ecfeb766e19e231173d935d
apb12 <apbapb12@yahoo.com>
2019-06-27 10:48:17 +0300

    MainPageServlet and some fix

--- src/main/java/com/accenture/flowershop/fe/servlets/MainPageServlet.java
+++ src/main/java/com/accenture/flowershop/fe/servlets/MainPageServlet.java
@@ -10,18 +10,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @Controller
-public class LoginServlet extends HttpServlet {
+public class MainPageServlet extends HttpServlet {
 
     @Autowired
     private UserService userService;
-
     private ServletConfig config;
-
 
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -42,44 +39,26 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //logins(req,resp);
+        userSession(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        logins(req, resp);
+
     }
 
-    public void logins(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String login = req.getParameter("login");
-        String password = req.getParameter("password");
+    public void userSession(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = (String) req.getSession().getAttribute("login");
         resp.setContentType("text/html;charset=UTF-8");
         PrintWriter printWriter = resp.getWriter();
         printWriter.println("<html>");
         printWriter.println("<style>");
-        printWriter.println("body { background: url(images/img4.jpg); }");
+        printWriter.println("body { background: url(images/img2.jpg); }");
         printWriter.println("</style>");
         printWriter.println("<body>");
-
-        if (!login.isEmpty() && !password.isEmpty()) {
-            try {
-                if (userService.login(login, password)) {
-                    HttpSession session=req.getSession();
-                    session.setAttribute("login",login);
-                    resp.sendRedirect("MainPageServlet");
-                } else {
-                    printWriter.println("<h1 align=center>Такого логина или пароля не существует</h1>");
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            printWriter.println("<h1 align=center>Вы заполнили не все поля,попробуйте еще раз</h1>");
-        }
-        printWriter.println("<form action='login.jsp'>");
-        printWriter.println("<p align=center><input type=submit name='loginbutton' value='повторная авторизация'/></p>");
-        printWriter.println("</form>");
+        printWriter.println("<h1 align=center>Привет " + login + " вы в сессии</h1>");
         printWriter.println("</body>");
         printWriter.println("</html>");
     }
 }
+
