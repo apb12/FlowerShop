315aabb53b8e58f2fa7c135f3bbdad50518ff9ef
apb12 <apbapb12@yahoo.com>
2019-07-19 09:54:38 +0300

    timing aded to flowerstockwebserviceclient

--- src/main/java/com/accenture/flowershop/test/FlowerStockWebServiceClient.java
+++ src/main/java/com/accenture/flowershop/test/FlowerStockWebServiceClient.java
@@ -1,7 +1,6 @@
 package com.accenture.flowershop.test;
 
 import com.accenture.flowershop.ws.FlowerStockWebService;
-import com.accenture.flowershop.ws.FlowerStockWebServiceImpl;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
@@ -9,11 +8,16 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class FlowerStockWebServiceClient {
-    public static void main(String[] args) throws MalformedURLException {
-        URL url=new URL("http://localhost:8083/flowershop/ws/FlowerStockWebService?wsdl");
-        QName qName=new QName("http://ws.flowershop.accenture.com/","FlowerStockWebServiceImplService");
-        Service service =Service.create(url,qName);
-        FlowerStockWebService fs=service.getPort(FlowerStockWebService.class);
-        fs.increaseFlowerStockSize(100);
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+        URL url = new URL("http://localhost:8083/flowershop/ws/FlowerStockWebService?wsdl");
+        QName qName = new QName("http://ws.flowershop.accenture.com/", "FlowerStockWebServiceImplService");
+        Service service = Service.create(url, qName);
+        FlowerStockWebService fs = service.getPort(FlowerStockWebService.class);
+        while (true) {
+            long count = (long) (Math.random() * 20 + 10);
+            fs.increaseFlowerStockSize(count);
+            Thread.sleep(600000);
+            System.out.println(count);
+        }
     }
 }
